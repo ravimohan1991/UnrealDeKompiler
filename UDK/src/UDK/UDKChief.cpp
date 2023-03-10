5c503932c20afa636a948143006331f73bb0dd21
trueGriff <ravimohan1991@gmail.com>
2023-03-10 22:40:22 +0530

    better formatting and some @see test

--- UDK/src/UDK/UDKChief.cpp
+++ UDK/src/UDK/UDKChief.cpp
@@ -1,7 +1,7 @@
 #include <wx/wxprec.h>
 
 #ifndef WX_PRECOMP
-    #include <wx/wx.h>
+#include <wx/wx.h>
 #endif
 
 /** @file
@@ -12,94 +12,116 @@
 
 class MyApp : public wxApp
 {
-
 public:
-    /**
-     * @brief Create new frame and display that
-     *
-     * This routine creates new frame and displays the same.
-     *
-     * @return true on successful initialization of "hello world" application
-     * @see wxTopLevelWindowMSW::Show()
-     */
-    virtual bool OnInit();
+	/**
+	 * @brief Create new frame and display that
+	 *
+	 * This routine creates new frame and displays the same.
+	 *
+	 * @return true on successful initialization of "hello world" application
+	 * @see wxTopLevelWindowMSW::Show()
+	 */
+	virtual bool OnInit();
 };
- 
+
 class MyFrame : public wxFrame
 {
 public:
-    /**
-     * @brief Frame setup
-     *
-     * Sets and populates menubar with File and Help menus. File contains Hello and
-     * Quit entries while Help contains About entry. A status bar at the bottom is
-     * also created. A relevant bind to the OnHello, OnExit, and OnAbout routines
-     * is done for desired callback.
-     *
-     * @see MyFrame::OnHello(wxCommandEvent&)
-     * @see MyFrame::OnExit()
-     * @see MyFrame::OnAbout(wxCommandEvent& event)
-     */
-    MyFrame();
- 
+	/**
+	 * @brief Frame setup
+	 *
+	 * Sets and populates menubar with File and Help menus. File contains Hello and \n
+	 * Quit entries while Help contains About entry. A status bar at the bottom is \n
+	 * also created. A relevant bind to the OnHello, OnExit, and OnAbout routines \n
+	 * is done for desired callback.
+	 *
+	 * @see MyFrame::OnHello(wxCommandEvent&)
+	 * @see MyFrame::OnExit()
+	 * @see MyFrame::OnAbout(wxCommandEvent& event)
+	 */
+	MyFrame();
+
 private:
-    void OnHello(wxCommandEvent& event);
-    void OnExit(wxCommandEvent& event);
-    void OnAbout(wxCommandEvent& event);
+	/**
+	 * @brief Hello entry callback
+	 *
+	 * Opens up a modal message window with desired hello message.
+	 *
+	 * @see MyFrame::MyFrame()
+	 */
+	void OnHello(wxCommandEvent& event);
+
+	/**
+	 * @brief Exit entry callback
+	 *
+	 * Closes the application.
+	 *
+	 * @see MyFrame::MyFrame()
+	 */
+	void OnExit(wxCommandEvent& event);
+
+	/**
+	 * @brief About entry callback
+	 *
+	 * Opens up a modal message window with desired application information.
+	 *
+	 * @see MyFrame::MyFrame()
+	 */
+	void OnAbout(wxCommandEvent& event);
 };
- 
+
 enum
 {
-    ID_Hello = 1
+	ID_Hello = 1
 };
- 
+
 wxIMPLEMENT_APP_CONSOLE(MyApp);
- 
+
 bool MyApp::OnInit()
 {
-    MyFrame *frame = new MyFrame();
-    frame->Show(true);
-    return true;
+	MyFrame* frame = new MyFrame();
+	frame->Show(true);
+	return true;
 }
- 
+
 MyFrame::MyFrame()
-    : wxFrame(nullptr, wxID_ANY, "Hello World")
+	: wxFrame(nullptr, wxID_ANY, "Hello World")
 {
-    wxMenu *menuFile = new wxMenu;
-    menuFile->Append(ID_Hello, "&Hello...\tCtrl-H",
-                     "Help string shown in status bar for this menu item");
-    menuFile->AppendSeparator();
-    menuFile->Append(wxID_EXIT);
- 
-    wxMenu *menuHelp = new wxMenu;
-    menuHelp->Append(wxID_ABOUT);
- 
-    wxMenuBar *menuBar = new wxMenuBar;
-    menuBar->Append(menuFile, "&File");
-    menuBar->Append(menuHelp, "&Help");
- 
-    SetMenuBar( menuBar );
- 
-    CreateStatusBar();
-    SetStatusText("Welcome to wxWidgets!");
- 
-    Bind(wxEVT_MENU, &MyFrame::OnHello, this, ID_Hello);
-    Bind(wxEVT_MENU, &MyFrame::OnAbout, this, wxID_ABOUT);
-    Bind(wxEVT_MENU, &MyFrame::OnExit, this, wxID_EXIT);
+	wxMenu* menuFile = new wxMenu;
+	menuFile->Append(ID_Hello, "&Hello...\tCtrl-H",
+		"Help string shown in status bar for this menu item");
+	menuFile->AppendSeparator();
+	menuFile->Append(wxID_EXIT);
+
+	wxMenu* menuHelp = new wxMenu;
+	menuHelp->Append(wxID_ABOUT);
+
+	wxMenuBar* menuBar = new wxMenuBar;
+	menuBar->Append(menuFile, "&File");
+	menuBar->Append(menuHelp, "&Help");
+
+	SetMenuBar(menuBar);
+
+	CreateStatusBar();
+	SetStatusText("Welcome to wxWidgets!");
+
+	Bind(wxEVT_MENU, &MyFrame::OnHello, this, ID_Hello);
+	Bind(wxEVT_MENU, &MyFrame::OnAbout, this, wxID_ABOUT);
+	Bind(wxEVT_MENU, &MyFrame::OnExit, this, wxID_EXIT);
 }
- 
+
 void MyFrame::OnExit(wxCommandEvent& event)
 {
-    Close(true);
+	Close(true);
 }
- 
+
 void MyFrame::OnAbout(wxCommandEvent& event)
 {
-    wxMessageBox("This is a wxWidgets Hello World example",
-                 "About Hello World", wxOK | wxICON_INFORMATION);
+	wxMessageBox("This is a wxWidgets Hello World example",
+		"About Hello World", wxOK | wxICON_INFORMATION);
 }
- 
+
 void MyFrame::OnHello(wxCommandEvent& event)
 {
-    wxLogMessage("Hello world from wxWidgets!");
+	wxLogMessage("Hello world from wxWidgets!");
 }
