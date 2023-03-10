8e64b0dbebbb87b98a882058c198bd06974ab5ac
ravimohan1991 <ravimohan1991@users.noreply.github.com>
2023-03-10 17:16:21 +0000

    deploy: d834da0f4cd1ec023206092bcaa7fb50c3e2864c

--- search/functions_1.js
+++ search/functions_1.js
@@ -1,4 +1,7 @@
 var searchData=
 [
-  ['oninit_0',['OnInit',['../class_my_app.html#a79fa75d1155f0e85e20f2869538296d6',1,'MyApp']]]
+  ['onabout_0',['OnAbout',['../class_my_frame.html#ab470cba0c5d8d46980c602589d556ee5',1,'MyFrame']]],
+  ['onexit_1',['OnExit',['../class_my_frame.html#ab72daefc5a74695f02427ae126df7261',1,'MyFrame']]],
+  ['onhello_2',['OnHello',['../class_my_frame.html#a86b48287b527870ff56fedf9515ff9d6',1,'MyFrame']]],
+  ['oninit_3',['OnInit',['../class_my_app.html#a79fa75d1155f0e85e20f2869538296d6',1,'MyApp']]]
 ];
