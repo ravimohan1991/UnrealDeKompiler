fad771f773a62bb37e44e0cf9ad1086a54f28c81
trueGriff <ravimohan1991@gmail.com>
2023-03-10 19:54:47 +0530

    minor update

--- UDK/src/UDK/UDKChief.cpp
+++ UDK/src/UDK/UDKChief.cpp
@@ -28,7 +28,7 @@ public:
      *
      * Create new frame and display that
      *
-     * @return true on successful initialization
+     * @return true on successful initialization of application
      */
     virtual bool OnInit();
 };
